9a087251529ea5de97db70797ad75e52245fb83c
Jihwan-Hong <124516776+flareseek@users.noreply.github.com>
2023-07-13 17:49:47 +0900

    [Silver III] Title: 피보나치 함수, Time: 76 ms, Memory: 11472 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -5,16 +5,16 @@ public class Main {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    int[][] FB = new int[41][2];
+    int[][] FB = new int[2][41];
     FB[0][0] = 1; FB[0][1] = 0; FB[1][0] = 0; FB[1][1] = 1;
-    for(int i = 2; i < FB.length; i++) {
-      FB[i][0] = FB[i-1][0] + FB[i-2][0];
-      FB[i][1] = FB[i-1][1] + FB[i-2][1];
+    for(int i = 2; i < 41; i++) {
+      FB[0][i] = FB[0][i-1] + FB[0][i-2];
+      FB[1][i] = FB[1][i-1] + FB[1][i-2];
     }
     int N = Integer.parseInt(br.readLine());
     while(N-->0) {
       int t = Integer.parseInt(br.readLine());
-      bw.write(FB[t][0] + " " + FB[t][1]);
+      bw.write(FB[0][t] + " " + FB[1][t]);
       bw.newLine();
     }
     bw.flush();
