c7d475c7b6a735182769a6bbf24b4192edb9a479
Jihwan-Hong <124516776+flareseek@users.noreply.github.com>
2023-07-30 19:08:47 +0900

    [Silver IV] Title: ATM, Time: 240 ms, Memory: 18784 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -5,15 +5,12 @@ import java.util.stream.Stream;
 public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    br.readLine();
+    int T = Integer.parseInt(br.readLine());
     int[] N = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
     br.close();
     Arrays.sort(N);
-    int sum = 0, t = 0;
-    for(int i = 0; i < N.length; i++) {
-      t += N[i];
-      sum += t;
-    }
+    int sum = 0;
+    for(int i = 0; i < T; i++) sum += N[i] * (T - i);
     System.out.println(sum);
   }
 }
