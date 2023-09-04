a11ef4cef7ff43fda148b6a3dc67f69c37f23768
Jihwan-Hong <124516776+flareseek@users.noreply.github.com>
2023-09-04 09:27:13 +0900

    [Silver V] Title: 뒤집기, Time: 76 ms, Memory: 11528 KB -BaekjoonHub

---
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    char[] C = br.readLine().toCharArray();
+    int count = 0;
+    int last = -1;
+    for(char c: C) {
+      if (last != c) {
+        count ++;
+        last = c;
+      }
+    }
+    System.out.println(count > 0 ? count/2 : 0);
+    br.close();
+  }
+}
