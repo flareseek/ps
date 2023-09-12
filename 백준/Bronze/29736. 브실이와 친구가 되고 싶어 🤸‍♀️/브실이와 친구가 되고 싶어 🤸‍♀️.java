431680c9b792149acfcc3a5d24e53b7d28d9514e
Jihwan-Hong <124516776+flareseek@users.noreply.github.com>
2023-09-12 22:10:41 +0900

    [Bronze IV] Title: 브실이와 친구가 되고 싶어 🤸‍♀️, Time: 76 ms, Memory: 11488 KB -BaekjoonHub

---
+++ 백준/Bronze/29736. 브실이와 친구가 되고 싶어 🤸‍♀️/브실이와 친구가 되고 싶어 🤸‍♀️.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    final int A = Integer.parseInt(st.nextToken());
+    final int B = Integer.parseInt(st.nextToken());
+
+    st = new StringTokenizer(br.readLine()) ;
+    final int K = Integer.parseInt(st.nextToken());
+    final int X = Integer.parseInt(st.nextToken());
+    br.close();
+
+    final int C = K - X > 0 ? K - X : 0;
+    final int D = K + X;
+
+    int f = C < A ? A : C, s = B < D ? B : D;
+    System.out.println(s - f >= 0 ? s - f + 1 : "IMPOSSIBLE");
+  }
+}
