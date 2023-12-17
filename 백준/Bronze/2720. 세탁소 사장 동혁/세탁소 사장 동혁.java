29f590f42e3e545347a38c5b353d0f0da94249a5
Jihwan-Hong <124516776+flareseek@users.noreply.github.com>
2023-12-17 15:07:21 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 80 ms, Memory: 11344 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        while(N-->0) {
+            int money = Integer.parseInt(br.readLine());
+            sb.append(money/25).append(" ");
+            sb.append(money%25/10).append(" ");
+            sb.append(money%25%10/5).append(" ");
+            sb.append(money%25%10%5).append("\n");
+        }
+        System.out.print(sb);
+        br.close();
+    }
+}
