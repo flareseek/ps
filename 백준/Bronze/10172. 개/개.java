1cdc2ff94258e16dd80cf391ace1b55c44295c89
Jihwan-Hong <124516776+flareseek@users.noreply.github.com>
2023-12-30 21:39:24 +0900

    [Bronze V] Title: 개, Time: 72 ms, Memory: 11248 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.print("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+    }
+}
