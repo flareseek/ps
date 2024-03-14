bb9636bf23b2adfd1c13bcafcd591d43b4206ad6
Jihwan-Hong <124516776+flareseek@users.noreply.github.com>
2024-03-14 22:51:37 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 16 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.swift
@@ -0,0 +1,23 @@
+let n = Int(readLine()!)!
+for i in 1..<n {
+    for _ in 0..<(n-i) {
+        print(" ", terminator: "")
+    }
+    for _ in 0..<(i) {
+        print("*", terminator: "")
+    }
+    print()
+}
+for i in 0..<n {
+    print("*", terminator: "")
+}
+print()
+for i in 1..<n {
+    for _ in 0..<(i) {
+        print(" ", terminator: "")
+    }
+    for _ in 0..<(n-i) {
+        print("*", terminator: "")
+    }
+    print()
+}
