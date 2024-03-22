fcf71d179199223422a628e9ab999d6105baef77
Jihwan-Hong <124516776+flareseek@users.noreply.github.com>
2024-03-22 23:52:37 +0900

    [Bronze IV] Title: Telemarketer or not?, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/16017. Telemarketer or not？/Telemarketer or not？.swift
@@ -0,0 +1,10 @@
+let n1 = Int(readLine()!)!
+let n2 = Int(readLine()!)!
+let n3 = Int(readLine()!)!
+let n4 = Int(readLine()!)!
+
+if (n1 == 8 || n1 == 9) && (n2 == n3) && (n4 == 8 || n4 == 9) {
+    print("ignore")
+} else {
+    print("answer")
+}
