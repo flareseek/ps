148d5635dfa5792904b643f8a7d05b3929912fc4
Jihwan-Hong <124516776+flareseek@users.noreply.github.com>
2024-10-28 22:07:55 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(void) {
+    int n, a;
+    std::cin >> n;
+    while(n--) {
+        std::cin >> a;
+        std::cout << a << " " << a << "\n";
+    }
+    return 0;
+}
