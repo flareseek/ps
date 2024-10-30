cf0c17982b8a845b9a971afdee7a3193d5c1a848
Jihwan-Hong <124516776+flareseek@users.noreply.github.com>
2024-10-30 22:13:44 +0900

    [Bronze V] Title: Serca, Time: 96 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(void) {
+    int n;
+    std::cin >> n;
+    while (n--) {
+        std::cout << " @@@   @@@\n";
+        std::cout << "@   @ @   @\n";
+        std::cout << "@    @    @\n";
+        std::cout << "@         @\n";
+        std::cout << " @       @ \n";
+        std::cout << "  @     @  \n";
+        std::cout << "   @   @   \n";
+        std::cout << "    @ @    \n";
+        std::cout << "     @     \n";
+    }
+    return 0;
+}
