67acdb93b3509eee233716bdb5baf4a6f8349f0f
jihwan-hong <124516776+flareseek@users.noreply.github.com>
2026-01-12 15:08:28 +0900

    [Bronze II] Title: 직사각형을 만드는 방법, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8320. 직사각형을 만드는 방법/직사각형을 만드는 방법.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n, cnt = 0;
+  cin >> n;
+  for (int i = 1; i <= n; i++)
+    for (int j = i; j <=n; j++)
+      if (i*j <= n) cnt ++;
+
+  cout << cnt;
+  return 0;
+}
